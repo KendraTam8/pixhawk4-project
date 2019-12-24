feabde851b1e9763714761df6a975a5018aef343
Kendra Tam <ktam@kendrat.am>
2019-12-23 22:18:52 -0500

    add photosensor

--- src/FirstProject.ino
+++ src/FirstProject.ino
@@ -6,22 +6,36 @@
  */
 
 int led1 = D6;
-int led2 = D7;
+//int led2 = D7;
+
+int photosensor = A0; // This is where your photoresistor or phototransistor is plugged in. The other side goes to the "power" pin (below).
+int analogvalue; // Here we are declaring the integer variable analogvalue, which we will use later to store the value of the photoresistor or phototransistor.
+
+int ledToggle(String command); // Forward declaration
+int ledToggle(String command); // Forward declaration
 
 // setup() runs once, when the device is first turned on.
 void setup() {
+	// This is here to allow for debugging using the USB serial port
+    Serial.begin();
+
 	// Put initialization like pinMode and begin functions here.
 	pinMode(led1, OUTPUT);
-	pinMode(led2, OUTPUT);
+	//pinMode(led2, OUTPUT);
 
-   // We are also going to declare a Particle.function so that we can turn the LED on and off from the cloud.
-   Particle.function("led",ledToggle);
-   // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
-	
-   // For good measure, let's also make sure both LEDs are off when we start:
-   digitalWrite(led1, LOW);
-   digitalWrite(led2, LOW);
-}
+	digitalWrite(led1, HIGH);
+	 // We are going to declare a Particle.variable() here so that we can access the value of the photosensor from the cloud.
+    Particle.variable("analogvalue", &analogvalue, INT);
+    // This is saying that when we ask the cloud for "analogvalue", this will reference the variable analogvalue in this app, which is an integer variable.
+
+	// We are also going to declare a Particle.function so that we can turn the LED on and off from the cloud.
+	Particle.function("led",ledToggle);
+	// This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
+
+	// For good measure, let's also make sure both LEDs are off when we start:
+	// digitalWrite(led1, LOW);
+	// digitalWrite(led2, LOW);
+	}
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
@@ -44,9 +58,15 @@ void loop() {
 	// And repeat!
 	*/
 
+	// check to see what the value of the photoresistor or phototransistor is and store it in the int variable analogvalue
+    analogvalue = analogRead(photosensor);
 
+    // This prints the value to the USB debugging serial port (for optional debugging purposes)
+    Serial.printlnf("%d", analogvalue);
 
-
+    // This delay is just to prevent overflowing the serial buffer, plus we really don't need to read the sensor more than
+    // 10 times per second (100 millisecond delay)
+    delay(100);
 }
 
 
@@ -64,12 +84,12 @@ int ledToggle(String command) {
 
     if (command=="on") {
         digitalWrite(led1,HIGH);
-        digitalWrite(led2,HIGH);
+        //digitalWrite(led2,HIGH);
         return 1;
     }
     else if (command=="off") {
         digitalWrite(led1,LOW);
-        digitalWrite(led2,LOW);
+        //digitalWrite(led2,LOW);
         return 0;
     }
     else {
