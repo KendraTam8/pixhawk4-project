786c70c7fe3c9c9068cd4271d8a1b096fe8c750c
Kendra Tam <ktam@kendrat.am>
2019-12-23 20:42:33 -0500

    blinking light example

--- src/FirstProject.ino
+++ src/FirstProject.ino
@@ -1,18 +1,35 @@
 /*
  * Project FirstProject
- * Description:
- * Author:
- * Date:
+ * Description: Blinking LED
+ * Author: Kendra Tam
+ * Date: December 23, 2019
  */
+
+int led1 = D7;
 
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
-
+	// Put initialization like pinMode and begin functions here.
+	pinMode(led1, OUTPUT);
+	//pinMode(led2, OUTPUT);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
+	// The core of your code will likely live here.
+	// To blink the LED, first we'll turn it on...
+	digitalWrite(led1, HIGH);
+	//digitalWrite(led2, HIGH);
 
+	// We'll leave it on for 1 second...
+	delay(1000);
+
+	// Then we'll turn it off...
+	digitalWrite(led1, LOW);
+	//digitalWrite(led2, LOW);
+
+	// Wait 1 second...
+	delay(1000);
+
+	// And repeat!
 }
