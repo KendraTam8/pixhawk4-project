0ea622ba1f0d4dd4d61e17f1bb2104f93b7278f2
Kendra Tam <ktam@kendrat.am>
2020-01-02 14:15:21 -0500

    send mavlink data between arduino and pixhawk

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -1,19 +1,15 @@
-#include <checksum.h>
 #include <mavlink.h>
-#include <mavlink_helpers.h>
-#include <mavlink_types.h>
-#include <protocol.h>
 #include <SoftwareSerial.h>
 
 unsigned long previousMillisMAVLink = 0;     // will store last time MAVLink was transmitted and listened
 unsigned long next_interval_MAVLink = 1000;  // next interval to count
 const int num_hbs = 60;                      // # of heartbeats to wait before activating STREAMS from Pixhawk. 60 = one minute.
 int num_hbs_pasados = num_hbs;
-SoftwareSerial mySerial(2,3);
+SoftwareSerial mySerial(2,3);   //RX, TX
 
 void setup() {
-  mySerial.begin(57600);
-  Serial.begin(9600);
+  mySerial.begin(9600);
+  Serial.begin(57600);
 }
 
 void loop() {
@@ -21,8 +17,8 @@ void loop() {
   /* The default UART header for your MCU */ 
   int sysid = 1;                   ///< ID 20 for this airplane. 1 PX, 255 ground station
   int compid = 158;                ///< The component sending the message
-  int type = MAV_TYPE_QUADROTOR;   ///< This system is an airplane / fixed wing
- 
+  int type = MAV_TYPE_QUADROTOR; 
+  
   // Define the system type, in this case an airplane -> on-board controller
   uint8_t system_type = MAV_TYPE_GENERIC;
   uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
@@ -36,7 +32,7 @@ void loop() {
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
  
   // Pack the message
-  mavlink_msg_heartbeat_pack(1,0, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
+  mavlink_msg_heartbeat_pack(1, 0, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
  
   // Copy the message to the send buffer
   uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
@@ -49,7 +45,7 @@ void loop() {
     // Timing variables
     previousMillisMAVLink = currentMillisMAVLink;
 
-    mySerial.write(buf, len);
+    Serial.write(buf, len);
 
     //Mav_Request_Data();
     num_hbs_pasados++;
@@ -90,10 +86,17 @@ void Mav_Request_Data()
    */
 
   // To be setup according to the needed information to be requested from the Pixhawk
-  const int  maxStreams = 2;
-  const uint8_t MAVStreams[maxStreams] = {MAV_DATA_STREAM_EXTENDED_STATUS, MAV_DATA_STREAM_EXTRA1};
-  const uint16_t MAVRates[maxStreams] = {0x02,0x05};
-    
+  // const int  maxStreams = 2;
+  // const uint8_t MAVStreams[maxStreams] = {MAV_DATA_STREAM_EXTENDED_STATUS, MAV_DATA_STREAM_EXTRA1};
+  // const uint16_t MAVRates[maxStreams] = {0x02,0x05};
+  uint8_t _system_id = 20; // id of computer which is sending the command (ground control software has id of 255)
+  uint8_t _component_id = 158; // seems like it can be any # except the number of what Pixhawk sys_id is
+  uint8_t _target_system = 1; // Id # of Pixhawk (should be 1)
+  uint8_t _target_component = MAV_COMP_ID_ALL; // Target component, 0 = all (seems to work with 0 or 1
+  uint8_t _req_stream_id = MAV_DATA_STREAM_ALL;
+  uint16_t _req_message_rate = 0x01; //number of times per second to request the data in hex
+  uint8_t _start_stop = 1; //1 = start, 0 = stop  
+  /*
   for (int i=0; i < maxStreams; i++) {
     /*
      * mavlink_msg_request_data_stream_pack(system_id, component_id, 
@@ -106,23 +109,29 @@ void Mav_Request_Data()
      *    uint8_t target_system, uint8_t target_component, uint8_t req_stream_id, 
      *    uint16_t req_message_rate, uint8_t start_stop)
      * 
-     */
-    mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAVStreams[i], MAVRates[i], 1);
+     *
+    mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAV_DATA_STREAM_ALL, 0x02, 1);
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-    mySerial.write(buf, len);
+    Serial.write(buf, len);
   }
+  */
+  mavlink_msg_request_data_stream_pack(2, 200, &msg, _target_system, _target_component, _req_stream_id, _req_message_rate, _start_stop);
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+  Serial.write(buf, len);
+
 }
 
 void comm_receive() {
   mavlink_message_t msg;
   mavlink_status_t status;
 
-  while(mySerial.available()>0) {
-    uint8_t c = mySerial.read();
-
+  while(Serial.available()>0) {
+    uint8_t c = Serial.read();
+    //mySerial.println(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status));
+    
     // Try to get a new message
     if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
-
+      mySerial.println("hi"+msg.msgid);
       // Handle message
       switch(msg.msgid) {
         case MAVLINK_MSG_ID_HEARTBEAT:  // #0: Heartbeat
@@ -171,14 +180,27 @@ void comm_receive() {
              */
             mavlink_attitude_t attitude;
             mavlink_msg_attitude_decode(&msg, &attitude);
-
-            if(attitude.roll>1) leds_modo = 0;
-            else if(attitude.roll<-1) leds_modo = 2;
-            else leds_modo=1;
+//
+//            if(attitude.roll>1) leds_modo = 0;
+//            else if(attitude.roll<-1) leds_modo = 2;
+//            else leds_modo=1;
           }
           break;
 
-        
+        case MAVLINK_MSG_ID_GPS_RAW_INT:
+          {
+            mavlink_gps_raw_int_t packet;
+            mavlink_msg_gps_raw_int_decode(&msg, &packet);
+            
+            mySerial.print("\nGPS Fix: ");mySerial.println(packet.fix_type);
+            mySerial.print("GPS Latitude: ");mySerial.println(packet.lat);
+            mySerial.print("GPS Longitude: ");mySerial.println(packet.lon);
+            mySerial.print("GPS Speed: ");mySerial.println(packet.vel);
+            mySerial.print("Sats Visible: ");mySerial.println(packet.satellites_visible);
+          
+          }
+          break;
+      
        default:
           break;
       }
